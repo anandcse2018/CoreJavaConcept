7a7e61617671777dd13811c3bcdaee28351f18fc
Anand Maurya <anandkumarmaurya@cdac.in>
2026-03-12 16:51:41 +0530

    for each loop

---
+++ src/com/corejava/SecondSexy.java
@@ -0,0 +1,16 @@
+package com.corejava;
+
+import java.util.Scanner;
+
+public class SecondSexy {
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner("System.in");
+		
+		int num;
+		System.out.print("Please Enter Number : ");
+		num = in.nextInt();
+		System.out.println("Number : "+num);
+	}
+
+}
--- src/com/corejava/functional/ForEach.java
+++ src/com/corejava/functional/ForEach.java
@@ -1,5 +1,20 @@
 package com.corejava.functional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForEach {
+	
+	public static void main(String[] args) 
+	{
+		List<Integer> list = new ArrayList<>();
+		list.add(190);
+		list.add(180);
+		list.add(30);
+		list.add(20);
+		list.add(120);
+		
+		list.forEach(x->System.out.println(x));
+	}
 
 }
