c63b1a97911ab67331d310fbe032715440da50b3
Anand Maurya <anandkumarmaurya@cdac.in>
2026-03-21 08:49:56 +0530

    .

--- src/com/corejava/SecondSexy.java
+++ src/com/corejava/SecondSexy.java
@@ -1,16 +1,19 @@
 package com.corejava;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SecondSexy {
 	public static void main(String[] args) 
 	{
-		Scanner in = new Scanner("System.in");
+		List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+		list1.forEach(System.out::println);
 		
-		int num;
-		System.out.print("Please Enter Number : ");
-		num = in.nextInt();
-		System.out.println("Number : "+num);
+		List<Integer> list2 = new LinkedList<>();
+		list2.addAll(Arrays.asList(1,2,3,4,5));
+		list2.forEach(System.out::print);
 	}
 
 }
