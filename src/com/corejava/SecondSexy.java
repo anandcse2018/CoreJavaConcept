a67b9adc05b9c8b206546039c3184ff982ea66d3
anand <anandcse2018@gmail.com>
2026-03-24 08:10:28 +0530

    .

--- src/com/corejava/SecondSexy.java
+++ src/com/corejava/SecondSexy.java
@@ -2,32 +2,24 @@ package com.corejava;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 public class SecondSexy 
 {
 	public static void main(String[] args) 
 	{
-	    List<Integer> list2 = new ArrayList<>(); 
-	    list2.addAll(Arrays.asList(0,1,2,3,4,5,6));
-	    
-	    ListIterator<Integer> it = list2.listIterator();
-	    while(it.hasNext())
-	    {
-	    	int x = it.next();
-	    	if(x == 4)
-	    	{
-	    		it.remove();
-	    		it.set(19);
-	    		
-	    		it.add(9);
-	    		
-	    		
-	    	}
-	    	else
-	    		System.out.println(x);
-	    }
-	    System.out.println(list2);
+		List<Integer> list1 =new ArrayList<>();
+		list1.addAll(Arrays.asList(1,2,3,4,5,6,7));
+		list1.stream()
+		.filter(x-> x<7)
+		.map(x-> x%2==0?x:(x+1))
+		.sorted(Comparator.reverseOrder())
+		.reduce(null);
+		
+		System.out.println(list1);
 	}
 }
